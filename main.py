51ee13a5dacc765be6296b272381119f0a35edb0
Miłosz Roman <mioszroman@gmail.com>
2022-03-16 18:52:04 +0100

    Add multiplication funcion

--- main.py
+++ main.py
@@ -28,3 +28,16 @@ def substraction(num):
     logging.warning("Odejmuje %s" % numbers)
     logging.info(f"Wynik ostateczny to {'%.2f' % subs}" )
     return subs
+
+def multiplication(num):
+    how_many = int(input("Ile  liczby chcesz pomnozyć: "))
+    numbers = []
+    for i in range(how_many):
+        i = float(input("Podaj składnik %s: " % (i+1)))
+        numbers.append(i)
+        mul = 1
+        for n in numbers:
+            mul = mul * n
+    logging.warning(f"Mnoze {numbers}")
+    logging.info(f"Wynik to {'%.2f' % mul}")
+    return mul
