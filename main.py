22406a517b6c666d2bd1d22a4ddd8288fc11ac1a
Miłosz Roman <mioszroman@gmail.com>
2022-03-16 18:50:01 +0100

    Add addition funcion

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 num = 0
+
+def addition(num):
+    how_many = int(input("Ile  liczby chcesz dodać: "))
+    numbers = []
+    add = 0
+    for i in range(how_many):
+        i = float(input("Podaj składnik %s: " % (i+1)))
+        numbers.append(i)
+        add = sum(numbers)
+    logging.warning(f"Dodaję {numbers}")
+    logging.info(f"Wynik to {'%.2f' % add}")
+    return add
