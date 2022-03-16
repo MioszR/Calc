b28db1ad7750c52bb5317121d6570a72e8791e61
Miłosz Roman <mioszroman@gmail.com>
2022-03-16 18:51:13 +0100

    Add substraction funcion

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ def addition(num):
     logging.warning(f"Dodaję {numbers}")
     logging.info(f"Wynik to {'%.2f' % add}")
     return add
+
+def substraction(num):
+    how_many = int(input("Ile  liczby chcesz odjąć: "))
+    numbers = []
+    for i in range(how_many):
+        i = float(input("Podaj składnik %s: " % (i+1)))
+        numbers.append(i)
+        subs = numbers[0]
+        for n in numbers[1:]:
+            subs = subs - n    
+            logging.info("Wynik to %d" % subs)
+
+    logging.warning("Odejmuje %s" % numbers)
+    logging.info(f"Wynik ostateczny to {'%.2f' % subs}" )
+    return subs
